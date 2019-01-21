d716deab12f150605c0d15e356debeb07099383c
Lahssen K <>
2019-01-21 23:38:01 +0100

    better tests

--- net/http/middlewares/auth_test.go
+++ net/http/middlewares/auth_test.go
@@ -11,29 +11,30 @@ func TestAuthMiddleware(t *testing.T) {
 	h := func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "Hi")
 	}
-	/*srv := httptest.NewServer(authHeaderMiddleware(h))
-		defer srv.Close()
-
-		tests:=[]struct{
-	url string
-		}{
-			{}
-		}
-		http.Get(test.url)
-	*/
-
-	w1 := httptest.NewRecorder()
-	w2 := httptest.NewRecorder()
-	r1 := httptest.NewRequest("GET", "http://localhost:8080/protected", nil)
-	r2 := httptest.NewRequest("GET", "http://localhost:8080/protected", nil)
-	r2.Header.Set("Authorization", "Bearer Kl09i009312lkdasja9-kdjskdj")
 	mid := authHeaderMiddleware(http.HandlerFunc(h))
-	mid.ServeHTTP(w1, r1)
-	if w1.Code != http.StatusUnauthorized {
-		t.Errorf("request without Authorization header did not fail!")
+	tests := []struct {
+		authHeader string
+		expCode    int
+		//expBody string
+	}{
+		{
+			authHeader: "", expCode: http.StatusUnauthorized,
+		},
+		{
+			authHeader: "Bearer Kl09i009312lkdasja9-kdjskdj", expCode: http.StatusOK,
+		},
 	}
-	mid.ServeHTTP(w2, r2)
-	if w2.Code != http.StatusOK {
-		t.Errorf("request with Authorization header is not OK!")
+
+	for ind, test := range tests {
+		w := httptest.NewRecorder()
+		r, _ := http.NewRequest("GET", "http://localhost:8080/protected", nil)
+		if test.authHeader != "" {
+			r.Header.Set("Authorization", "Bearer Kl09i009312lkdasja9-kdjskdj")
+		}
+		mid.ServeHTTP(w, r)
+		if w.Code != test.expCode {
+			t.Errorf("test %d: expected code %d received %d", ind, test.expCode, w.Code)
+		}
 	}
+
 }
