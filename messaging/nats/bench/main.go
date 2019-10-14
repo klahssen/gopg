536908904dd5dd552cfdf41b34581ad2b1865398
Lahssen Kh <>
2019-10-15 00:01:47 +0200

    natsb benchmark pubsub N:M one subj

--- messaging/nats/bench/main.go
+++ messaging/nats/bench/main.go
@@ -15,36 +15,17 @@ const (
 )
 
 func main() {
-	f()
+	//pubsub()
+	reqresp()
 }
 
-func f() {
+func pubsub() {
 	nPubsFlag := flag.Uint("p", 1, "number of publishers")
 	nConsumersFlag := flag.Uint("c", 1, "number of consumers")
 	nMsgsFlag := flag.Uint("m", 1000000, "number of messages")
 	msgSizeFlag := flag.Uint("ms", 128, "number of publishers")
 	flag.Parse()
 
-	msg := make([]byte, int(*msgSizeFlag))
-	wg1 := sync.WaitGroup{}
-	wg1.Add(int(*nPubsFlag))
-	for i := 1; i <= int(*nPubsFlag); i++ {
-		go func(name string) {
-			defer wg1.Done()
-			target := int(*nMsgsFlag)
-			nc, err := nats.Connect(nats.DefaultURL)
-			if err != nil {
-				log.Fatal(err)
-			}
-			t0 := time.Now()
-			for j := 1; j < target; j++ {
-				nc.Publish(subj, msg)
-			}
-			nc.Flush()
-			dur := time.Since(t0)
-			log.Printf("%s published %d messages in %s (%.1f/sec)", name, *nMsgsFlag, dur, float64(time.Second)/float64(dur)*float64(target))
-		}(fmt.Sprintf("publisher %d", i))
-	}
 	wg2 := sync.WaitGroup{}
 	wg2.Add(int(*nConsumersFlag))
 	for i := 1; i <= int(*nConsumersFlag); i++ {
@@ -83,6 +64,172 @@ func f() {
 			dur := time.Since(t0)
 			log.Printf("%s consumed %d messages in %s (%.1f/sec)", name, target, dur, float64(time.Second)/float64(dur)*float64(target))
 		}(fmt.Sprintf("consumer %d", i))
+	}
+	msg := make([]byte, int(*msgSizeFlag))
+	wg1 := sync.WaitGroup{}
+	wg1.Add(int(*nPubsFlag))
+	for i := 1; i <= int(*nPubsFlag); i++ {
+		go func(name string) {
+			defer wg1.Done()
+			target := int(*nMsgsFlag)
+			nc, err := nats.Connect(nats.DefaultURL)
+			if err != nil {
+				log.Fatal(err)
+			}
+			t0 := time.Now()
+			for j := 1; j < target; j++ {
+				nc.Publish(subj, msg)
+			}
+			nc.Flush()
+			dur := time.Since(t0)
+			log.Printf("%s published %d messages in %s (%.1f/sec)", name, *nMsgsFlag, dur, float64(time.Second)/float64(dur)*float64(target))
+		}(fmt.Sprintf("publisher %d", i))
+	}
+	wg1.Wait()
+	wg2.Wait()
+	//fmt.Printf("%d publisher(s) published %d messages each in %s", *nPubsFlag, *nMsgsFlag, time.Since(t0))
+}
+
+func reqresp() {
+	nPubsFlag := flag.Uint("p", 1, "number of publishers")
+	nConsumersFlag := flag.Uint("c", 1, "number of consumers")
+	nMsgsFlag := flag.Uint("m", 1000000, "number of messages")
+	msgSizeFlag := flag.Uint("ms", 128, "number of publishers")
+	flag.Parse()
+
+	wg2 := sync.WaitGroup{}
+	wg2.Add(int(*nConsumersFlag))
+	for i := 1; i <= int(*nConsumersFlag); i++ {
+		go func(name string) {
+			defer wg2.Done()
+			nc, err := nats.Connect(nats.DefaultURL)
+			if err != nil {
+				log.Fatal(err)
+			}
+			n := 0
+
+			t0 := time.Now()
+			sub, _ := nc.SubscribeSync(subj)
+			target := int(*nMsgsFlag) * int(*nPubsFlag)
+
+			for {
+				m, _ := sub.NextMsg(time.Millisecond * 100)
+				m.Respond([]byte("world"))
+				n++
+				//log.Printf("%s received %d msg", name, n)
+				if n == target {
+					break
+				}
+			}
+
+			/*
+				done := make(chan int)
+				nc.Subscribe(subj, func(m *nats.Msg) {
+					n++
+					//log.Printf("%s received %d msg", name, n)
+					if n == int(*nMsgsFlag) {
+						close(done)
+					}
+				})
+				<-done
+			*/
+			dur := time.Since(t0)
+			log.Printf("%s consumed %d messages in %s (%.1f/sec)", name, target, dur, float64(time.Second)/float64(dur)*float64(target))
+		}(fmt.Sprintf("consumer %d", i))
+	}
+	msg := make([]byte, int(*msgSizeFlag))
+	wg1 := sync.WaitGroup{}
+	wg1.Add(int(*nPubsFlag))
+	for i := 1; i <= int(*nPubsFlag); i++ {
+		go func(name string) {
+			defer wg1.Done()
+			target := int(*nMsgsFlag)
+			nc, err := nats.Connect(nats.DefaultURL)
+			if err != nil {
+				log.Fatal(err)
+			}
+			t0 := time.Now()
+			j := 0
+			for j < target {
+				//nc.Publish(subj, msg)
+				_, err := nc.Request(subj, msg, time.Second*2)
+				/*if err != nil {
+					log.Printf("[ERROR] %v", err)
+				} else if resp == nil {
+					log.Printf("[ERROR] response is nil")
+				} else {
+					log.Println(string(resp.Data))
+				}*/
+				if err == nil {
+					j++
+				}
+			}
+			nc.Flush()
+			dur := time.Since(t0)
+			log.Printf("%s published %d messages in %s (%.1f/sec)", name, *nMsgsFlag, dur, float64(time.Second)/float64(dur)*float64(target))
+		}(fmt.Sprintf("publisher %d", i))
+	}
+	wg1.Wait()
+	wg2.Wait()
+	//fmt.Printf("%d publisher(s) published %d messages each in %s", *nPubsFlag, *nMsgsFlag, time.Since(t0))
+}
+
+func f2() {
+	nPubsFlag := flag.Uint("p", 1, "number of publishers")
+	nConsumersFlag := flag.Uint("c", 1, "number of consumers")
+	nMsgsFlag := flag.Uint("m", 1000000, "number of messages")
+	msgSizeFlag := flag.Uint("ms", 128, "number of publishers")
+	flag.Parse()
+
+	wg2 := sync.WaitGroup{}
+	wg2.Add(int(*nConsumersFlag))
+	for i := 1; i <= int(*nConsumersFlag); i++ {
+		go func(name string) {
+			defer wg2.Done()
+			nc, err := nats.Connect(nats.DefaultURL)
+			if err != nil {
+				log.Fatal(err)
+			}
+			n := 0
+
+			t0 := time.Now()
+			//sub, _ := nc.SubscribeSync(subj)
+			target := int(*nMsgsFlag) * int(*nPubsFlag)
+
+			done := make(chan int)
+			nc.Subscribe(subj, func(m *nats.Msg) {
+				n++
+				//log.Printf("%s received %d msg", name, n)
+				if n == target {
+					close(done)
+				}
+			})
+			<-done
+
+			dur := time.Since(t0)
+			log.Printf("%s consumed %d messages in %s (%.1f/sec)", name, target, dur, float64(time.Second)/float64(dur)*float64(target))
+		}(fmt.Sprintf("consumer %d", i))
+	}
+
+	msg := make([]byte, int(*msgSizeFlag))
+	wg1 := sync.WaitGroup{}
+	wg1.Add(int(*nPubsFlag))
+	for i := 1; i <= int(*nPubsFlag); i++ {
+		go func(name string) {
+			defer wg1.Done()
+			target := int(*nMsgsFlag)
+			nc, err := nats.Connect(nats.DefaultURL)
+			if err != nil {
+				log.Fatal(err)
+			}
+			t0 := time.Now()
+			for j := 1; j < target; j++ {
+				nc.Publish(subj, msg)
+			}
+			nc.Flush()
+			dur := time.Since(t0)
+			log.Printf("%s published %d messages in %s (%.1f/sec)", name, *nMsgsFlag, dur, float64(time.Second)/float64(dur)*float64(target))
+		}(fmt.Sprintf("publisher %d", i))
 	}
 	wg1.Wait()
 	wg2.Wait()
---
+++ messaging/nats/natsb/main.go
@@ -0,0 +1,164 @@
+// Copyright 2015-2019 The NATS Authors
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+	"strconv"
+	"sync"
+	"time"
+
+	"github.com/nats-io/nats.go"
+	//"github.com/nats-io/nats.go/bench"
+)
+
+// Some sane defaults
+const (
+	DefaultNumMsgs     = 500000
+	DefaultNumPubs     = 1
+	DefaultNumSubs     = 0
+	DefaultMessageSize = 128
+)
+
+func usage() {
+	log.Printf("Usage: nats-bench [-s server (%s)] [-np NUM_PUBLISHERS] [-ns NUM_SUBSCRIBERS] [-n NUM_MSGS] [-ms MESSAGE_SIZE] <subject>\n", nats.DefaultURL)
+	flag.PrintDefaults()
+}
+
+func showUsageAndExit(exitcode int) {
+	usage()
+	os.Exit(exitcode)
+}
+
+func main() {
+	var urls = flag.String("s", nats.DefaultURL, "The nats server URLs (separated by comma)")
+	var numPubs = flag.Int("np", DefaultNumPubs, "Number of Concurrent Publishers")
+	var numSubs = flag.Int("ns", DefaultNumSubs, "Number of Concurrent Subscribers")
+	var numMsgs = flag.Int("n", DefaultNumMsgs, "Number of Messages to Publish")
+	var msgSize = flag.Int("ms", DefaultMessageSize, "Size of the message.")
+	var showHelp = flag.Bool("h", false, "Show help message")
+
+	log.SetFlags(0)
+	flag.Usage = usage
+	flag.Parse()
+
+	if *showHelp {
+		showUsageAndExit(0)
+	}
+
+	args := flag.Args()
+	if len(args) != 1 {
+		showUsageAndExit(1)
+	}
+
+	if *numMsgs <= 0 {
+		log.Fatal("Number of messages should be greater than zero.")
+	}
+
+	//benchmark = bench.NewBenchmark("NATS", *numSubs, *numPubs)
+
+	var startwg sync.WaitGroup
+	var donewg sync.WaitGroup
+
+	donewg.Add(*numPubs + *numSubs)
+
+	// Run Subscribers first
+	startwg.Add(*numSubs)
+	for i := 0; i < *numSubs; i++ {
+		nc, err := nats.Connect(*urls)
+		if err != nil {
+			log.Fatalf("Can't connect: %v\n", err)
+		}
+		defer nc.Close()
+
+		go runSubscriber(strconv.Itoa(i+1), nc, &startwg, &donewg, *numMsgs, *msgSize)
+	}
+	startwg.Wait()
+
+	// Now Publishers
+	startwg.Add(*numPubs)
+	//pubCounts := bench.MsgsPerClient(*numMsgs, *numPubs)
+	for i := 0; i < *numPubs; i++ {
+		nc, err := nats.Connect(*urls)
+		if err != nil {
+			log.Fatalf("Can't connect: %v\n", err)
+		}
+		defer nc.Close()
+
+		//go runPublisher(nc, &startwg, &donewg, pubCounts[i], *msgSize)
+		go runPublisher(strconv.Itoa(i+1), nc, &startwg, &donewg, (*numMsgs)/(*numPubs), *msgSize)
+	}
+
+	log.Printf("Starting benchmark [msgs=%d, msgsize=%d, pubs=%d, subs=%d]\n", *numMsgs, *msgSize, *numPubs, *numSubs)
+
+	startwg.Wait()
+	donewg.Wait()
+
+	//benchmark.Close()
+
+	//fmt.Print(benchmark.Report())
+}
+
+func runPublisher(name string, nc *nats.Conn, startwg, donewg *sync.WaitGroup, numMsgs int, msgSize int) {
+	startwg.Done()
+
+	args := flag.Args()
+	subj := args[0]
+	var msg []byte
+	if msgSize > 0 {
+		msg = make([]byte, msgSize)
+	}
+
+	start := time.Now()
+
+	for i := 0; i < numMsgs; i++ {
+		nc.Publish(subj, msg)
+	}
+	nc.Flush()
+	dur := time.Since(start)
+	//benchmark.AddPubSample(bench.NewSample(numMsgs, msgSize, start, time.Now(), nc))
+	log.Printf("[PUBLISHER] %s: pub %d messages in %s (%.1fmsgs/sec)", name, numMsgs, dur, float64(numMsgs)*float64(time.Second)/float64(dur))
+	donewg.Done()
+}
+
+func runSubscriber(name string, nc *nats.Conn, startwg, donewg *sync.WaitGroup, numMsgs int, msgSize int) {
+	defer nc.Close()
+	args := flag.Args()
+	subj := args[0]
+
+	received := 0
+	ch := make(chan time.Time, 2)
+	_, _ = nc.Subscribe(subj, func(msg *nats.Msg) {
+		received++
+		if received == 1 {
+			ch <- time.Now()
+		}
+		if received >= numMsgs {
+			ch <- time.Now()
+		}
+	})
+	//sub.SetPendingLimits(-1, -1)
+	nc.Flush()
+	startwg.Done()
+
+	<-ch //start
+	start := time.Now()
+	<-ch //end
+	dur := time.Since(start)
+	//benchmark.AddSubSample(bench.NewSample(numMsgs, msgSize, start, end, nc))
+	log.Printf("[SUBSCRIBER] %s: consumed %d messages in %s (%.1fmsgs/sec)", name, numMsgs, dur, float64(numMsgs)*float64(time.Second)/float64(dur))
+	donewg.Done()
+}
